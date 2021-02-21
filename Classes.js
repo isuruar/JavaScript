25fbe65cbf83698df9f85130d18f0a8e89ee06a7
Isuru <32997733+isuruar@users.noreply.github.com>
2021-02-21 23:25:56 +0530

    Update Classes.js

--- Classes.js
+++ Classes.js
@@ -1,28 +1,31 @@
 // Classes are a template for creating objects. They encapsulate data with code to work on that data. 
 // Classes in JS are built on prototypes but also have some syntax and semantics that are not shared with ES5 class-like semantics.
 
+// constructor is a function that will execute immediately when a new color is created
+
+// methods can be created inside classes
 
 class Color {
-	constructor(r, g, b, name) {
+	constructor(r,g,b,name){
 		this.r = r;
 		this.g = g;
 		this.b = b;
 		this.name = name;
 	}
-	innerRGB() {
-		const { r, g, b } = this;
-		return `${r}, ${g}, ${b}`;
+	innerRGB (){
+		const {r,g,b} = this;
+		return `${r},${g},${b}`;
 	}
-	rgb() {
-		return `rgb(${this.innerRGB()})`;
+	rbg(){
+		return `rbg(${this.innerRGB()})`;
 	}
-	rgba(a = 1.0) {
-		return `rgba(${this.innerRGB()}, ${a})`;
+	rgba(a = 1.0){
+		return `rgba(${this.innerRGB()},${a})`;
 	}
-	hex() {
-		const { r, g, b } = this;
+	hex(){
+		const {r,g,b} = this;
 		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+			'#' + ((1 << 24)) + (r << 16) + (g << 8) + b.toString(16).slice(1)
 		);
 	}
 }
