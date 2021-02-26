2638af4ef5a903ab620fce14da56d0506a0db648
Isuru <32997733+isuruar@users.noreply.github.com>
2021-02-26 12:55:04 +0530

    Create app.js

---
+++ DOM Events/RandomColors/app.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+button.addEventListener('click', function () {
+    const newColor = makeRandColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const buttons = document.querySelector('button');
+
+for (let button of buttons){
+    button.addEventListener('click',function (){
+        button.style.backgroundColor = makeRandColor();
+    })
+}
+
+
+
