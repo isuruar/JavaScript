5a31b982dab50305383566c219f482189afe0ece
Isuru <32997733+isuruar@users.noreply.github.com>
2021-02-26 12:52:24 +0530

    Create app.js

---
+++ DOM Events/Delegation/app.js
@@ -0,0 +1,32 @@
+/*Capturing and bubbling allow us to implement one of most powerful event handling patterns called event delegation.
+
+The idea is that if we have a lot of elements handled in a similar way, then instead of assigning a handler to each of them – we put a single handler on their common ancestor.
+
+In the handler we get event.target to see where the event actually happened and handle it.*/
+
+
+
+const tweetForm = document.querySelector('#tweetForm');
+const tweetsContainer = document.querySelector('#tweets');
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(usernameInput.value, tweetInput.value)
+    usernameInput.value = '';
+    tweetInput.value = '';
+});
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)
+    newTweet.append(bTag);
+    newTweet.append(`- ${tweet}`)
+    tweetsContainer.append(newTweet);
+}
+
+tweetsContainer.addEventListener('click', function (e) {
+    e.target.nodeName === 'LI' && e.target.remove();
+})
+
