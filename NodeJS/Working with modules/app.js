b2d7ed1ddcb9063a3eff0b6f8ebc71f49761d904
Isuru <32997733+isuruar@users.noreply.github.com>
2021-03-04 11:21:14 +0530

    Update app.js

--- NodeJS/Working with modules/app.js
+++ NodeJS/Working with modules/app.js
@@ -5,3 +5,10 @@ console.log(tutorial.sum(1,1));
 console.log(tutorial.PI);
 console.log(new tutorial.SomeMathObject());
 
+const { PI, square } = require('./math');
+
+const cats = require('./shelter')
+// console.log(PI)
+
+// console.log(square(9))
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats)
